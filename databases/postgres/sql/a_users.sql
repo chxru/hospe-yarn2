d7b4d09a555a199ca07329f5ccde1d89ab8fee4a
Charuka Samarakoon <charuka@protonmail.com>
2022-05-08 01:42:51 +0530

    refactor(iam-service): :sparkles: Split auth data from user common data

--- databases/postgres/sql/a_users.sql
+++ databases/postgres/sql/a_users.sql
@@ -3,7 +3,15 @@ create schema if not exists users;
 create table if not exists users.data(
   user_id uuid primary key default gen_random_uuid(),
   email text unique not null,
-  pwd text not null,
+  fname text not null,
+  lname text,
   created_at timestamptz not null default now(),
   updated_at timestamptz not null default now()
+);
+
+create table if not exists users.auth(
+  email text unique primary key,
+  pwd text not null,
+  user_id uuid not null references users.data(user_id),
+  last_update timestamptz not null default now()
 );
